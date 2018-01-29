b7cf0fac5a91445e93554a2e8432fe02285e0057
f0reachARR <f0reach@f0reach.me>
2018-01-29 23:20:47 +0900

    ページ移動時のSlot無効化

--- src/views/reducers/slot.ts
+++ src/views/reducers/slot.ts
@@ -11,6 +11,8 @@ export const slot = (state: StoreSlot = initialState, action: Actions) => {
             return { ...state, slot: action.payload, isFailed: false };
         case 'FETCH_FAILED_SLOT':
             return { ...state, isFailed: true, slot: undefined };
+        case INVALIDATE_SLOT:
+            return { ...state, slot: undefined };
         default:
             return state;
     }
