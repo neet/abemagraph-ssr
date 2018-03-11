7f4b8e85a45fcea4af6b5bb272ab7ea659a4d23e
f0reachARR <f0reach@f0reach.me>
2018-03-11 12:51:05 +0900

    Mark分離

--- src/views/components/Mark.tsx
+++ src/views/components/Mark.tsx
@@ -1,14 +1,5 @@
 import * as React from 'react';
-
-const markName = {
-    'first': '初',
-    last: '終',
-    live: '生',
-    bingeWatching: '一挙',
-    recommendation: '注目',
-    drm: 'DRM',
-    newcomer: '新'
-};
+import { markName } from '../constant/const';
 
 export const Mark = ({ mark, showItem }: { mark: string[], showItem: string[] }) =>
     <>{showItem.filter(m => mark.indexOf(m) >= 0).map(m => <mark key={m}>{markName[m]}</mark>)}</>;
