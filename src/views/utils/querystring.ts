00184488f48cce54bded0142550594f075fb9d62
f0reachARR <f0reach@f0reach.me>
2018-03-14 01:27:09 +0900

    querystring修正

--- src/views/utils/querystring.ts
+++ src/views/utils/querystring.ts
@@ -1,7 +1,7 @@
 export const parse = (qs: string): { [key: string]: string } => {
     return qs.replace(/^\?/, '').split('&').reduce((obj, item) => {
         const [key, value] = item.split('=').map(s => decodeURI(s));
-        obj[key] = unescape(value || '');
+        obj[key] = value.replace('+', ' ');
         return obj;
     }, {});
 };
