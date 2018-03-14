ead3c72080e79c96b59ec0d4b902832325d77665
f0reachARR <f0reach@f0reach.me>
2018-03-14 12:52:29 +0900

    Markで型チェック

--- src/views/components/Mark.tsx
+++ src/views/components/Mark.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { markName } from '../constant/const';
+import { markName, MarkType } from '../constant/const';
 
-export const Mark = ({ mark, showItem }: { mark: string[], showItem: string[] }) =>
+export const Mark = ({ mark, showItem }: { mark: string[], showItem: MarkType[] }) =>
     <>{showItem.filter(m => mark.indexOf(m) >= 0).map(m => <mark key={m}>{markName[m]}</mark>)}</>;
